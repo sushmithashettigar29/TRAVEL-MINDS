da4a9d2db0fbcd979f63953fa25a47cd09dbfa29
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-05-19 19:04:33 +0530

    Add Project Files

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.slider');
+    sidebar.style.display = 'flex';
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.slider');
+    sidebar.style.display = 'none';
+}
